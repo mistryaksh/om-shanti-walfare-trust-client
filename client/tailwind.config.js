3321b909858e48e44498ca75c73aa2e55033baff
Anil Prajapati <anil100699@gmail.com>
2024-02-10 01:45:28 +0530

    modified-tailwind-config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/**/*.{js,jsx,ts,tsx}'
+  ],
   theme: {
-    extend: {},
+    extend: {
+      'emerald': {
+        '50': '#ebfef4',
+        '100': '#cffce2',
+        '200': '#a4f6cb',
+        '300': '#69ecb0',
+        '400': '#2dda90',
+        '500': '#09cc7f',
+        '600': '#009c61',
+        '700': '#007d51',
+        '800': '#026342',
+        '900': '#035138',
+        '950': '#002e20',
+      },
+    },
   },
   plugins: [],
 }
