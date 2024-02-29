87838cc66efebb513add9571e74d853a68177d3e
mistryaksh <111272027+mistryaksh@users.noreply.github.com>
2024-02-29 18:47:02 +0530

    added terms & policy

--- src/core/components/Layout/Footer/top.tsx
+++ src/core/components/Layout/Footer/top.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC } from "react";
 import { Link } from "react-router-dom";
 
 import { IHeaderLink } from "../../../interface";
@@ -8,64 +8,64 @@ import { ROUTES } from "../../../enums";
 const STYLES = LAYOUT_STYLES;
 
 export const FooterTop: FC = () => {
-   const links: IHeaderLink[] = [
-      {
-         name: "About US",
-         to: ROUTES.ABOUT,
-      },
-      {
-         name: "Latest Programs",
-         to: ROUTES.PROGRAMS
-      },
-      {
-         name: "Social Events",
-         to: ROUTES.EVENTS
-      },
-      {
-         name: "Our Blogs",
-         to: ROUTES.BLOGS
-      },
-      {
-         name: "Contact US",
-         to: ROUTES.CONTACT
-      },
-   ];
+     const links: IHeaderLink[] = [
+          {
+               name: "About US",
+               to: ROUTES.ABOUT,
+          },
+          {
+               name: "Latest Programs",
+               to: ROUTES.PROGRAMS,
+          },
+          {
+               name: "Social Events",
+               to: ROUTES.EVENTS,
+          },
+          {
+               name: "Our Blogs",
+               to: ROUTES.BLOGS,
+          },
+          {
+               name: "Contact US",
+               to: ROUTES.CONTACT,
+          },
 
-   return (
-      <div className="grid grid-cols-12 gap-x-20 gap-y-8 mb-10 sm:mb-28 w-full">
-         <div className="col-span-12 sm:col-span-3">
-            <img src="/transparent-logo.png" alt="logo" className="w-20 sm:w-28" />
-         </div>
-         <div className="col-span-12 sm:col-span-4">
-            <h4 className="text-sm sm:text-lg font-medium text-white mb-3.5 sm:mb-6">Contact Info</h4>
-            <ul className="flex flex-col items-start gap-y-2.5 sm:gap-y-4 text-slate-500 text-xs sm:text-base font-light tracking-wide w-full">
-               <li>
-                  Address: 123, Lorem Ipsum, <br /> Dolor Sit Amet
-               </li>
-               <li>
-                  Phone: +91 99204 41626 / 93729 97878
-               </li>
-               <li>
-                  Email: omshantiwelfareturst@gmail.com
-               </li>
-            </ul>
-         </div>
-         <div className="col-span-12 sm:col-span-4">
-            <h4 className="text-sm sm:text-lg font-medium text-white mb-3 sm:mb-6">Important Links</h4>
-            <ul className="flex flex-col items-start gap-y-2.5 sm:gap-y-4">
-               {links.map((link, index) => (
-                  <li
-                     key={index}
-                     className={
-                        STYLES.LINK +
-                        ' text-slate-500 font-light tracking-wide'
-                     }
-                  >
-                     <Link to={link.to}>{link.name}</Link>
-                  </li>
-               ))}
-            </ul>
-         </div>
-      </div>
-   );
+          {
+               name: "Terms & Conditions",
+               to: ROUTES.TERMS_CONDITION,
+          },
+
+          {
+               name: "Refund Policy",
+               to: ROUTES.REFUND_POLICY,
+          },
+     ];
+
+     return (
+          <div className="grid grid-cols-12 gap-x-20 gap-y-8 mb-10 sm:mb-28 w-full">
+               <div className="col-span-12 sm:col-span-3">
+                    <img src="/transparent-logo.png" alt="logo" className="w-20 sm:w-28" />
+               </div>
+               <div className="col-span-12 sm:col-span-4">
+                    <h4 className="text-sm sm:text-lg font-medium text-white mb-3.5 sm:mb-6">Contact Info</h4>
+                    <ul className="flex flex-col items-start gap-y-2.5 sm:gap-y-4 text-slate-500 text-xs sm:text-base font-light tracking-wide w-full">
+                         <li>
+                              Address: 123, Lorem Ipsum, <br /> Dolor Sit Amet
+                         </li>
+                         <li>Phone: +91 99204 41626 / 93729 97878</li>
+                         <li>Email: omshantiwelfaretrust@gmail.com</li>
+                    </ul>
+               </div>
+               <div className="col-span-12 sm:col-span-4">
+                    <h4 className="text-sm sm:text-lg font-medium text-white mb-3 sm:mb-6">Important Links</h4>
+                    <ul className="flex flex-col items-start gap-y-2.5 sm:gap-y-4">
+                         {links.map((link, index) => (
+                              <li key={index} className={STYLES.LINK + " text-slate-500 font-light tracking-wide"}>
+                                   <Link to={link.to}>{link.name}</Link>
+                              </li>
+                         ))}
+                    </ul>
+               </div>
+          </div>
+     );
 };
