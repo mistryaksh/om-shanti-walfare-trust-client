ac017e44f42bafc28f87be2f196eede768315f8d
mistryaksh <111272027+mistryaksh@users.noreply.github.com>
2024-03-13 15:29:58 +0530

    Update server.utils.ts

--- src/core/utils/server.utils.ts
+++ src/core/utils/server.utils.ts
@@ -1,5 +1,5 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query";
 
 export const UserReduxSetup = fetchBaseQuery({
-        baseUrl: "https://om-shanti-backend-2-dz6i.onrender.com/api/1.0",
+        baseUrl: "https://om-shanti-walfare-trust-backend-o06r.onrender.com/api/1.0",
 });
